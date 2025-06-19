d737a855859be9c66fe9ee29a5744210187b2912
HOUDAED <houmasseedvige@gmail.com>
2025-06-19 20:33:14 +0100

    mis a jour de la gestion de la connexion à la base de données déplacée vers config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+window.config = {
+    supabase: {
+        url: 'https://snkldzuytinzfeczhpno.supabase.co',
+        anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNua2xkenV5dGluemZlY3pocG5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5MTY3OTIsImV4cCI6MjA2NTQ5Mjc5Mn0.FCwI_L1z-3yQfUtpESjFpRr2QDpR-anNDPUT5j2VfSM'
+    }
+};
